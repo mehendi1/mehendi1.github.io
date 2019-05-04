78d4fbb88ae387a008381cd0451e99c98ff8ce3a
mehendi1 <thanatus3@gmail.com>
2019-05-05 00:16:07 +0300

    miner

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $('#uk').on('click', function(){
+        document.documentElement.lang="uk";    
+    });
+
+    $('#ru').on('click', function(){
+        document.documentElement.lang="ru";    
+    });
+    
+    $('#en').on('click', function(){
+        document.documentElement.lang="en";    
+    });
+
+
+});
